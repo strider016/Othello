f322997c9756bc739818bd4178349ee06caf3fb9
Joachim Zetterman <joachimz@kth.se>
2015-10-14 12:44:04 +0200

    New class UI and new way to launch

---
+++ src/sample/UI.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+/**
+ *
+ * @author Zetterman
+ */
+public class UI extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Othello");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+}
