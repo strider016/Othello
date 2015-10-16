0e0323f37a2306e6f0c2dbaebeefafd24cf4cd85
Rasmus <razmus.jansson@gmail.com>
2015-10-16 22:31:48 +0200

    Fixat lite controller och länkat med fxml

--- src/sample/UI.java
+++ src/sample/UI.java
@@ -5,11 +5,13 @@
  */
 package sample;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
 
 import game.Game;
 /**
@@ -20,23 +22,56 @@ import game.Game;
 public class UI extends Application {
     
     private Game game;
+    private BorderPane rootPane;
+    private Stage primaryStage;
     
     public static void main(String[] args)  { 
         launch(args);
         
     }
     
-    
+    public void initBackgroundLayout(){
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(UI.class.getResource("sample.fxml"));
+            rootPane = loader.load();
+
+            Scene scene = new Scene(rootPane);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            Controller controller = loader.getController();
+            controller.setUI(this);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Othello");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
-        
-        
-        
-        
-        
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Othello");
+
+        initBackgroundLayout();
+    }
+
+    public String getPlayerOneName(){
+        //return game.getPlayerOneName();
+        return "Jeffrey";
+    }
+
+    public String getPlayerOneScore(){
+        //return game.getPlayerOneScore();
+        return "0";
+    }
+
+    public String getPlayerTwoName(){
+        //return game.getPlayerTwoScore();
+        return "Milley";
+    }
+
+    public String getPlayerTwoScore(){
+        //return game.getPlayerTwoScore();
+        return "0";
     }
 }
